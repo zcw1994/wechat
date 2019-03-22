cc3f372c8372de82cea687ac3f427524129823a7
king-queen <543728245@qq.com>
2019-03-22 16:57:36 +0800

    引入wechat模块实现你问我答

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const wechat = require('wechat');
 const app =express();
 
-app.get('/', (req,res) => {
-  res.send('hello world')
-})
+const config = {
+  token: 'weixin',
+  appid: 'wxeed4195f75522c32',
+  encodingAESKey: "yfiwga83QvDSbZQ0eqOYyGNs7TpkQDO8jpS0uWt9dyr",
+  checkSignature: true
+}
+app.use('/wechat',wechat(config, (req, res, next) => {
+  if (req.weixin.Content === '你好') {
+    res.reply('你好')
+  } else if(req.weixin.Content === '你真帅'){
+    res.reply('真有眼光')
+  } else {
+    res.reply(req.weixin.Content)
+  }
+}))
 
 
 
